2ba6caa54b57c7691a273a92ca61ac7165daeafa
Federico Carrozzino <federicocarrozzino98@gmail.com>
2024-07-31 00:36:46 +0200

    update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
+require('module-alias/register');
+
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 
 const db = admin.firestore();
+
+// Authentication
 
 export const getUserData = functions.https.onCall(async (_, context) => {
     if (!context.auth) {
@@ -53,76 +57,48 @@ export const getUserData = functions.https.onCall(async (_, context) => {
     }
 });
 
-export const getTalks = functions.https.onCall(async (_, context) => {
-    if (!context.auth) {
-        throw new functions.https.HttpsError('unauthenticated', 'User must be authenticated.');
-    }
+// Quiz
 
-    try {
-        const talksSnapshot = await db.collection('talks').get();
-
-        if (!talksSnapshot) {
-            throw new functions.https.HttpsError('not-found', 'Talks not found.');
-        }
-
-        const talks: any[] = [];
-
-        talksSnapshot.forEach(doc => {
-            const talkData = doc.data();
-            talks.push({
-                talkId: doc.id,
-                title: talkData.title,
-                description: talkData.description,
-                quizList: talkData.quizList,
-                maxScore: talkData.maxScore,
-            });
-        });
-
-        return { talks };
-    } catch (error) {
-        throw new functions.https.HttpsError('internal', 'An error occurred while fetching the talk quiz.', error);
-    }
-
-});
-
-export const getTalkQuiz = functions.https.onCall(async (data, context) => {
-    const { talkId } = data;
+export const getQuiz = functions.https.onCall(async (data, context) => {
+    const { quizId } = data;
 
     if (!context.auth) {
         throw new functions.https.HttpsError('unauthenticated', 'User must be authenticated.');
     }
 
-    if (!talkId) {
+    if (!quizId) {
         throw new functions.https.HttpsError('invalid-argument', 'The function must be called with a valid talkId.');
     }
 
     try {
-        const talkDoc = await db.collection('talks').doc(talkId).get();
+        const quizDoc = await db.collection('quizzes').doc(quizId).get();
 
-        if (!talkDoc.exists) {
-            throw new functions.https.HttpsError('not-found', 'Talk not found.');
+        if (!quizDoc.exists) {
+            throw new functions.https.HttpsError('not-found', 'Quiz not found.');
         }
 
-        const talkData = talkDoc.data();
+        const quizData = quizDoc.data();
 
-        if (!talkData || !talkData.isOpen) {
+        if (!quizData || !quizData.isOpen) {
             throw new functions.https.HttpsError('failed-precondition', 'Quiz is not open.');
         }
 
         return {
-            talkId: talkDoc.id,
-            quizList: talkData.quizList.map((quiz: any) => {
-                return { ...quiz, correctAnswer: null };
-            })
+            quizId: quizDoc.id,
+            questionList: quizData.questionList.map(
+                (question: { text: any; answerList: any; }) => ({
+                    text: question.text,
+                    answerList: question.answerList,
+                })
+            )
         };
     } catch (error) {
         throw new functions.https.HttpsError('internal', 'An error occurred while fetching the talk quiz.', error);
     }
 });
 
-
-export const submitQuizAnswer = functions.https.onCall(async (data, context) => {
-    const { talkId, answerList } = data;
+export const submitQuiz = functions.https.onCall(async (data, context) => {
+    const { quizId, answerList } = data;
 
     if (!context.auth) {
         throw new functions.https.HttpsError('unauthenticated', 'User must be authenticated.');
@@ -130,45 +106,45 @@ export const submitQuizAnswer = functions.https.onCall(async (data, context) => 
 
     const uid = context.auth.uid;
 
-    if (!talkId || !Array.isArray(answerList)) {
-        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with a valid talkId and answerList.');
+    if (!quizId || !Array.isArray(answerList)) {
+        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with a valid quizId and answerList.');
     }
 
     try {
-        const talkDoc = await db.collection('talksQuizzes').doc(talkId).get();
+        const quizDoc = await db.collection('quizzes').doc(quizId).get();
 
-        if (!talkDoc.exists) {
-            throw new functions.https.HttpsError('not-found', 'Talk not found.');
+        if (!quizDoc.exists) {
+            throw new functions.https.HttpsError('not-found', 'Quiz not found.');
         }
 
-        const talkData = talkDoc.data();
+        const quizData = quizDoc.data();
 
-        if (!talkData || !talkData.isOpen) {
+        if (!quizData || !quizData.isOpen) {
             throw new functions.https.HttpsError('failed-precondition', 'Quiz is not open.');
         }
 
-        const answerDoc = await db.collection('users').doc(uid).collection('talksQuizzes').doc(talkId).get();
+        const answerDoc = await db.collection('users').doc(uid).collection('quizzes').doc(quizId).get();
 
         if (answerDoc.exists) {
             throw new functions.https.HttpsError('already-exists', 'Quiz already submitted.');
         }
 
         let score = 0;
-        const maxScore = talkData.maxScore;
+        const maxScore = quizData.maxScore;
 
-        if (answerList.length !== talkData.quizList.length) {
+        if (answerList.length !== quizData.questionList.length) {
             throw new functions.https.HttpsError('invalid-argument', 'The answerList must have the same length as the quiz list.');
         }
 
-        talkData.quizList.forEach((quiz: any, index: number) => {
-            if (quiz.correctAnswer === answerList[index]) {
-                score += quiz.value;
+        quizData.questionList.forEach((question: any, index: number) => {
+            if (question.correctAnswer === answerList[index]) {
+                score += question.value;
             }
         });
 
         const result = { score, maxScore };
 
-        await db.collection('users').doc(uid).collection('talksQuizzes').doc(talkId).set(result);
+        await db.collection('users').doc(uid).collection('quizzes').doc(quizId).set(result);
 
         return result;
     } catch (error) {
@@ -176,3 +152,82 @@ export const submitQuizAnswer = functions.https.onCall(async (data, context) => 
     }
 });
 
+// Talks
+
+export const getTalkList = functions.https.onCall(async (_, context) => {
+    if (!context.auth) {
+        throw new functions.https.HttpsError('unauthenticated', 'User must be authenticated.');
+    }
+
+    const uid = context.auth.uid;
+
+    try {
+        const userDoc = await db.collection('users').doc(context.auth.uid).get();
+
+        if (!userDoc.exists) {
+            throw new functions.https.HttpsError('not-found', 'User not found.');
+        }
+
+        const userData = userDoc.data();
+
+        if (!userData) {
+            throw new functions.https.HttpsError('not-found', 'User data not found.');
+        }
+
+        const talksSnapshot = await db.collection('talks').get();
+
+        if (!talksSnapshot) {
+            throw new functions.https.HttpsError('not-found', 'Talks not found.');
+        }
+
+        const talkList = await Promise.all(
+            talksSnapshot.docs.map(
+                async talkDoc => {
+                    const talkData = talkDoc.data();
+
+                    const quizRef = talkData.quiz;
+
+                    const quizDoc = await quizRef.get()
+
+                    if (!quizDoc.exists) {
+                        throw new functions.https.HttpsError('not-found', 'Quiz not found.');
+                    }
+
+                    const quizData = quizDoc.data();
+
+                    const questionListRef = quizData.questionList;
+
+                    const questionListDoc = await questionListRef.get()
+
+                    if (!questionListDoc.exists) {
+                        throw new functions.https.HttpsError('not-found', 'Quiz not found.');
+                    }
+
+                    const questionListData = questionListDoc.data();
+
+                    const answerDoc = await db.collection('users').doc(uid).collection('quizzes').doc(quizDoc.id).get();
+
+                    const quiz = {
+                        quizId: quizDoc.id,
+                        questionList: questionListData
+                    };
+
+                    return {
+                        talkId: talkDoc.id,
+                        title: talkData.title,
+                        description: talkData.description,
+                        track: talkData.track,
+                        room: talkData.room,
+                        startTime: talkData.startTime,
+                        endTime: talkData.endTime,
+                        quiz: (answerDoc.exists) ? quiz : null,
+                    };
+                }
+            )
+        );
+
+        return talkList;
+    } catch (error) {
+        throw new functions.https.HttpsError('internal', 'An error occurred while fetching the talk quiz.', error);
+    }
+});
