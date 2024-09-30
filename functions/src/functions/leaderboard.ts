2b5388a96673ebaec452741688c094fdeb7a10fb
Domenico Rotolo <rotolonico@gmail.com>
2024-09-30 15:23:12 +0200

    Update leaderboard.ts

--- functions/src/functions/leaderboard.ts
+++ functions/src/functions/leaderboard.ts
@@ -37,6 +37,7 @@ export const getLeaderboard = functions.https.onCall(async (_, context) => {
                         group: null,
                         position: null,
                         score: userData.score,
+                        role: null
                     }
 
                     return leaderboardUserEntry;
