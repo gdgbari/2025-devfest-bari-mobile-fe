83847722f58c4eb7c93b3368bc061e0b3e0f2838
Federico Carrozzino <federicocarrozzino98@gmail.com>
2024-10-01 11:40:32 +0200

    welcome page ui 2.0

--- lib/ui/pages/welcome_page.dart
+++ lib/ui/pages/welcome_page.dart
@@ -10,43 +10,54 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: ColorPalette.black,
-        title: const Text(
-          'DevFest Bari 2024',
-          style: PresetTextStyle.white21w500,
-        ),
-        centerTitle: true,
+        backgroundColor: ColorPalette.pastelRed,
+        toolbarHeight: 0,
       ),
+      backgroundColor: ColorPalette.pastelRed,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: SvgPicture.asset(
-                  'assets/images/devfest_logo.svg',
-                  width: double.maxFinite,
+        bottom: false,
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 15,
+              child: SvgPicture.asset(
+                'assets/images/welcome.svg',
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.bottomCenter,
+              ),
+            ),
+            Flexible(
+              flex: 3,
+              child: Container(
+                color: Colors.white,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: OAuthButton(
+                      method: 'email',
+                      onPressed: () =>
+                          context.pushNamed(RouteNames.signUpRoute.name),
+                    ),
+                  ),
                 ),
               ),
-              const SizedBox(height: 20),
-              OAuthButton(
-                method: 'email',
-                onPressed: () => context.pushNamed(RouteNames.signUpRoute.name),
-              ),
-              // TODO: future implementation
-              // const SizedBox(height: 10),
-              // OAuthButton(
-              //   method: 'google',
-              //   onPressed: () => context.pushNamed(RouteNames.loginRoute.name),
-              // ),
-              // const SizedBox(height: 10),
-              // OAuthButton(
-              //   method: 'apple',
-              //   onPressed: () => context.pushNamed(RouteNames.loginRoute.name),
-              // ),
-            ],
-          ),
+            ),
+            Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).padding.bottom,
+            ),
+            // TODO: future implementation
+            // const SizedBox(height: 10),
+            // OAuthButton(
+            //   method: 'google',
+            //   onPressed: () => context.pushNamed(RouteNames.loginRoute.name),
+            // ),
+            // const SizedBox(height: 10),
+            // OAuthButton(
+            //   method: 'apple',
+            //   onPressed: () => context.pushNamed(RouteNames.loginRoute.name),
+            // ),
+          ],
         ),
       ),
     );
