09a6276d6c89b8dddd572e440630e0c31c25444f
Federico Carrozzino <federicocarrozzino98@gmail.com>
2024-10-07 12:26:08 +0200

    fix ios release + data deletion request

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -3,6 +3,7 @@ import 'package:devfest_bari_2024/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -31,6 +32,20 @@ class ProfilePage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         UserInfo(userProfile: state.userProfile),
+                        SizedBox(height: 5),
+                        GestureDetector(
+                          onTap: () => launchUrl(
+                            Uri.parse(
+                              'https://forms.gle/yAhTRZv6JKAYA3BY7',
+                            ),
+                          ),
+                          child: Text(
+                            'Request data deletion',
+                            style: PresetTextStyle.black13w500.copyWith(
+                              color: ColorPalette.coreRed,
+                            ),
+                          ),
+                        ),
                         Expanded(child: SizedBox()),
                         Center(child: UserQrCode(qrImage: qrImage)),
                         Expanded(child: SizedBox()),
